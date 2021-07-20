26fbc9f089d5e77d0a4e1ddaa3dcc6d17d858c46
Jackson Lessa <jacksonj.lessa@gmail.com>
2021-07-20 10:11:14 -0300

    ajustes para a front consumir o site

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
     public function register(Request $request){
         $fields = $request->validate([
             'name'=> 'required|string',
