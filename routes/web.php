4debc044227a364d8a7492105ad80a68c6d239f9
Jackson Lessa <jacksonj.lessa@gmail.com>
2021-10-01 17:36:51 -0300

    update docker, sera esse o final?

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::redirect('/', 'https://www.estetify.com.br/');
+Route::get('/', function () {
+    return 'Hello World';
+});
+// Route::redirect('/', 'https://www.estetify.com.br/');
 
