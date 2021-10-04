cbadf293dfeabb76c1f00a10f13164b15adfe791
Jackson Lessa <jacksonj.lessa@gmail.com>
2021-10-04 20:25:00 -0300

    adjusts init-letsencrypt

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(api.estetify.com.br)
 rsa_key_size=4096
 data_path="./docker/certbot"
 email="" # Adding a valid address is strongly recommended
